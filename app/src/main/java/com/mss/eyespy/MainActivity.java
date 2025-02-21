63dcc7ba442eb3d2172beff59e99e2cfc0034101
Joaquim <joamartins077@gmail.com>
2025-02-21 16:57:32 +0530

    cmd

--- app/src/main/java/com/mss/eyespy/MainActivity.java
+++ app/src/main/java/com/mss/eyespy/MainActivity.java
@@ -1,19 +1,81 @@
 package com.mss.eyespy;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout ll_Home, ll_Register, ll_ShiftTimings, ll_AssignShift, ll_Attendance, ll_Logout, ll_Exit ;
+    TextView tv_App_Ver_Up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.layoutdrawer);
+        menu = findViewById(R.id.main_menu);
+        ll_Home = findViewById(R.id.ll_Home);
+        ll_Register = findViewById(R.id.ll_Register);
+        ll_ShiftTimings = findViewById(R.id.ll_ShiftTimings);
+        ll_AssignShift = findViewById(R.id.ll_AssignShift);
+        ll_Attendance = findViewById(R.id.ll_Attendance);
+        ll_Logout = findViewById(R.id.ll_Logout);
+        ll_Exit = findViewById(R.id.ll_Exit);
+        tv_App_Ver_Up = findViewById(R.id.tv_App_Ver_Up);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrawar(drawerLayout);
+            }
+        });
+        ll_Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recreate();
+            }
+        });
+        ll_Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(MainActivity.this, SettingsActivity.class);
+            }
+        });
+    }
+
+    public static void openDrawar(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public  static void closeDrawer(DrawerLayout drawerLayout){
+        if(drawerLayout.isDrawerOpen((GravityCompat.START))){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public static  void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
     }
 }
